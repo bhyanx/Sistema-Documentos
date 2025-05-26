2eaa8cc73fbcfdf92c30807ca4c18e54e207f3c7
bhyanx <bhyanxdev@gmail.com>
2025-05-26 19:45:55 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+class Conectar {
+    
+    protected $dbh;
+
+    public function ConexionBdPracticante()
+    {
+        try {
+            $conexion = new PDO(
+                "mysql:host=metro.proxy.rlwy.net;port=31823;dbname=railway",
+                'root',
+                'KkjRAMPoRgXeUGERlhCQCZCuyWsGcVgl'
+            );
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conexion;
+        } catch (PDOException $e) {
+            // error_log("Error de conexión a bdPracticante: " . $e->getMessage(), 3, __DIR__ . '/../../logs/errors.log');
+            error_log("Error de conexión a bdPracticante: " . $e->getMessage());
+            throw $e;
+        }
+    }
+}
+?>
